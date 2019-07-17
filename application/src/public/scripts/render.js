a024bd4024bdd8c40d38c43726910ec597c5286c
kevparedes <kparede1@mail.sfsu.edu>
2019-07-16 20:38:43 -0700

    created render script

---
+++ application/src/public/scripts/render.js
@@ -0,0 +1,12 @@
+$( document ).ready(funtion() {
+ <% for (var i = 0; i < searchResult.length; i++) { %>
+    document.getElementById('result').innerHTML += '<p> <%= searchResult[i][ID] %>.<%= searchResult[i]['Name'] %></p>'
+    
+    document.getElementById('result').innerHTML += '<p> Issue Category: <%= searchResult[i]
+    ['Category'] %><br>Description: <% =searchResult[i]['Description'] %> </p>'
+    
+    document.getElementById('result').innerHTML += '<p>Date: <%= searchResult[i]
+    ['Date'] %><br>Location: <% =searchResult[i]['Location'] %> </p>'
+    <% } %>
+  });
+
