6e8511ca642d5abe17fc61b823583d184596ca6d
Erick <erickvelez7@gmail.com>
2019-07-29 12:44:07 -0700

    Add POST functions for search and login for /login

--- application/src/api/routes/login.js
+++ application/src/api/routes/login.js
@@ -4,8 +4,15 @@ router.get('/', function(req, res) {
     res.sendFile('public/pages/login.html', {root: './'});
 });
 
+/** POST function for search */
 router.post('/', function(req, res){
-    //TODO for registration
+    console.log("submitted search");
+    res.redirect(302, '/results');
+});
+
+/** POST function for login */
+router.post('/submit', function (req, res) {
+    console.log("submitted login");
 });
 
 module.exports = router;
