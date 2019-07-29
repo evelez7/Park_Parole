94951ef5c7fe5b5a7cc5cae5f1faf1a4519c5627
Erick <erickvelez7@gmail.com>
2019-07-28 18:18:08 -0700

    Fix body parser issue for queries

--- application/src/api/controllers/search.js
+++ application/src/api/controllers/search.js
@@ -1,4 +1,3 @@
-
 const db = require('../models/database.js');
 
 module.exports = {
@@ -7,24 +6,6 @@ module.exports = {
         var searchTerm = req.query.search;
         //user's selected category
         var category = req.query.category;
-        //users: function
-        var username = req.query.username || '';
-        // "myName"
-        var password = req.query.password || '';
-        // "password"
-
-        // encrypting users password
-        username = username.replace(/[!@#$%^&*]/g, '');
-        if (!username || !password || !users[username]) {
-            return res.sendStatus(400);
-          }
-          crypto.pbkdf2(password, users[username].salt, 10000, 512, function(err, hash) {
-            if (users[username].hash.toString() === hash.toString()) {
-              res.sendStatus(200);
-            } else {
-              res.sendStatus(401);
-            }
-          });
 
         let query = 'SELECT * FROM Issue';
         if (searchTerm != null && category != null) {
@@ -34,14 +15,14 @@ module.exports = {
         } else if (searchTerm == null && category != null) {
             query = `SELECT * FROM Issue WHERE Category = '` + category + `'`;
         }
-        db.query(query, (err, result) => {
+        db.query(query, (err, results, fields) => {
             if (err) { //TODO: properly handle errors
                 req.body.searchResult = "";
                 req.body.searchTerm = "";
                 req.body.category = "";
                 next();
             }
-            req.body.searchResult = result;
+            req.body.searchResult = results;
             req.body.searchTerm = searchTerm;
             req.body.category = category;
             next();
