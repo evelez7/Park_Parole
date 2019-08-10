a10deaf95772bb33ad85a3ebcb2d0b8250ee71d6
yungvinsantos <vinnysantos559@gmail.com>
2019-08-09 19:03:30 -0700

    changed image/thumbnail saving to server filesystem instead of databse blobs

--- application/src/controllers/postIssue.js
+++ application/src/controllers/postIssue.js
@@ -5,7 +5,10 @@
 */
 const db = require('../models/database.js');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const fs = require('file-system');
+const sharp = require('sharp');
+// const multer = require('multer');
+// const upload = multer({dest: '/uploads/'});
 
 module.exports = {
 	
@@ -14,9 +17,33 @@ module.exports = {
 	post : function (req, res, next) {
 			
 				//put name, email, and password into database
-				db.query('INSERT INTO Issue SET ?', {Park: req.body.park, Description: req.body.description, Category: req.body.category, Date: req.body.date, Status: "open"})
-				.then(([result, _]) => {
-					next();
-				})	
+				let data = req.file;
+
+				console.log(req.body);
+				console.log(req.body.description);
+				console.log(data);
+
+    			data = new Buffer(data.toString(), 'base64')
+    			let filename = new Date().toISOString().replace(/:/g, '-') + '.jpeg';
+
+				sharp(req.file.path)
+  				.rotate()
+  				.resize(200)
+  				.toBuffer()
+  				.then( data => { 
+					fs.writeFile(`./uploads/${filename}`, data, function(err) {
+        				if (err) console.log(err);
+    				})
+				
+					db.query('INSERT INTO Issue SET ?', {Park: req.body.park, Description: req.body.description,
+				 		Category: req.body.category, Image: 'http://34.68.209.201:3000/uploads/' + filename,  Date: req.body.date, Status: "open"})
+					.then(([result, _]) => {
+						next();
+					})
+				})
+				.catch( err => {
+					console.log(err);
+				})
+				
 	}
 }
